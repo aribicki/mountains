ad7ef2cb94d9fd1e3106a042c896c6374bdf4b44
Andrew Ribicki <aribicki@saintjoe.edu>
2015-02-27 08:41:31 -0600

    created new data for table

---
+++ moto.js
@@ -0,0 +1,17 @@
+// Andrew Ribicki
+// CMP237 
+// 2/27/2015
+// data for mountains repository for hw to show table
+
+var MOTO = [
+  {name: "HD Heritage Classic", year: 1992, rebuilt: "Yes"},
+  {name: "Honda CR", year: 1997, rebuilt: "Yes"},
+  {name: "DR-Z L", year: 2005, rebuilt: "JNo"},
+  {name: "HD Superglide Custom", year: 2012 , rebuilt: "No"},
+  {name: "KTM XC", year: 2013, rebuilt: "Yes"},
+  {name: "Honda CRF",  year: 2014, rebuilt: "No"},
+  {name: "Kawasaki KX", year: 2015, rebuilt: "No"}
+];
+
+if (typeof module != "undefined" && module.exports)
+  module.exports = MOTO;
