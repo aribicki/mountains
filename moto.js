6f13d71ac7a81a553357331b4667ce8134aeea79
Andrew Ribicki <aribicki@saintjoe.edu>
2015-03-20 08:39:59 -0500

    modified a little from my personal desktop

--- moto.js
+++ moto.js
@@ -4,13 +4,14 @@
 // data for mountains repository for hw to show table
 
 var MOTO = [
-  {name: "HD Heritage Classic", year: 1992, rebuilt: "Yes"},
-  {name: "Honda CR", year: 1997, rebuilt: "Yes"},
-  {name: "DR-Z L", year: 2005, rebuilt: "JNo"},
-  {name: "HD Superglide Custom", year: 2012 , rebuilt: "No"},
-  {name: "KTM XC", year: 2013, rebuilt: "Yes"},
-  {name: "Honda CRF",  year: 2014, rebuilt: "No"},
-  {name: "Kawasaki KX", year: 2015, rebuilt: "No"}
+  {name: "HD Heritage Classic", year: 1/1/1992, rebuilt: 1},
+  {name: "Honda CR", year: 1/1/1997, rebuilt: 1},
+  {name: "DR-Z L", year: 1/1/2005, rebuilt: 0},
+  {name: "HD Superglide Custom", year: 1/1/2012 , rebuilt: 0},
+  {name: "KTM XC", year: 1/1/2013, rebuilt: 1},
+  {name: "Honda CRF",  year: 1/1/2014, rebuilt: 0},
+  {name: "Kawasaki KX", year: 1/1/2015, rebuilt: 1}
+  {name: "Ducati", year: 1/1/2015, rebuilt: 0}
 ];
 
 if (typeof module != "undefined" && module.exports)
