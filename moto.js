62bbe4d2d4e506660f1fdf9f7263c4bc458b3ed9
Andrew Ribicki <aribicki@saintjoe.edu>
2015-03-20 08:43:24 -0500

    forgot a comma

--- moto.js
+++ moto.js
@@ -10,7 +10,7 @@ var MOTO = [
   {name: "HD Superglide Custom", year: 1/1/2012 , rebuilt: 0},
   {name: "KTM XC", year: 1/1/2013, rebuilt: 1},
   {name: "Honda CRF",  year: 1/1/2014, rebuilt: 0},
-  {name: "Kawasaki KX", year: 1/1/2015, rebuilt: 1}
+  {name: "Kawasaki KX", year: 1/1/2015, rebuilt: 1},
   {name: "Ducati", year: 1/1/2015, rebuilt: 0}
 ];
 
