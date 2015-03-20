6e53bc6d3764578993a6fc93bf4ec8f01c2252ba
Andrew Ribicki <aribicki@saintjoe.edu>
2015-03-20 09:50:36 -0500

    Update moto.js

--- moto.js
+++ moto.js
@@ -4,14 +4,14 @@
 // data for mountains repository for hw to show table
 
 var MOTO_FILE = [
-  {"name": "HD Heritage Classic", "year": 1/1/1992, "rebuilt": 150},
-  {"name": "Honda CR", "year": 1/1/1997, "rebuilt": 40},
-  {"name": "DR-Z L", "year": 1/1/2005, "rebuilt": 200},
-  {"name": "HD Superglide Custom", "year": 1/1/2012 , "rebuilt": 150},
-  {"name": "KTM XC", "year": 1/1/2013, "rebuilt": 40},
-  {"name": "Honda CRF",  "year": 1/1/2014, "rebuilt": 60},
-  {"name": "Kawasaki KX", "year": 1/1/2015, "rebuilt": 35},
-  {"name": "Ducati", "year": 1/1/2015, "rebuilt": 100}
+  {"name": "HD Heritage Classic", "lastrebuilt": 1/1/1992, "rebuilt": 150},
+  {"name": "Honda CR", "lastrebuilt": 1/1/1997, "rebuilt": 40},
+  {"name": "DR-Z L", "lastrebuilt": 1/1/2005, "rebuilt": 200},
+  {"name": "HD Superglide Custom", "lastrebuilt": 1/1/2012 , "rebuilt": 150},
+  {"name": "KTM XC", "lastrebuilt": 1/1/2013, "rebuilt": 40},
+  {"name": "Honda CRF",  "lastrebuilt": 1/1/2014, "rebuilt": 60},
+  {"name": "Kawasaki KX", "lastrebuilt": 1/1/2015, "rebuilt": 35},
+  {"name": "Ducati", "lastrebuilt": 1/1/2015, "rebuilt": 100}
 ];
 
 if (typeof module != "undefined" && module.exports)
