020e009dbfa0f0c044b5c77d90fa57d3600297c5
Andrew Ribicki <aribicki@saintjoe.edu>
2015-03-20 09:39:38 -0500

    Update moto.js

--- moto.js
+++ moto.js
@@ -4,15 +4,15 @@
 // data for mountains repository for hw to show table
 
 var MOTO_FILE = [
-  {name: "HD Heritage Classic", year: 1/1/1992, rebuilt: 150},
-  {name: "Honda CR", year: 1/1/1997, rebuilt: 40},
-  {name: "DR-Z L", year: 1/1/2005, rebuilt: 200},
-  {name: "HD Superglide Custom", year: 1/1/2012 , rebuilt: 150},
-  {name: "KTM XC", year: 1/1/2013, rebuilt: 40},
-  {name: "Honda CRF",  year: 1/1/2014, rebuilt: 60},
-  {name: "Kawasaki KX", year: 1/1/2015, rebuilt: 35},
-  {name: "Ducati", year: 1/1/2015, rebuilt: 100}
+  {"name": "HD Heritage Classic", "year": 1/1/1992, "rebuilt": 150},
+  {"name": "Honda CR", "year": 1/1/1997, "rebuilt": 40},
+  {"name": "DR-Z L", "year": 1/1/2005, "rebuilt": 200},
+  {"name": "HD Superglide Custom", "year": 1/1/2012 , "rebuilt": 150},
+  {"name": "KTM XC", "year": 1/1/2013, "rebuilt": 40},
+  {"name": "Honda CRF",  "year": 1/1/2014, "rebuilt": 60},
+  {"name": "Kawasaki KX", "year": 1/1/2015, "rebuilt": 35},
+  {"name": "Ducati", "year": 1/1/2015, "rebuilt": 100}
 ];
 
 if (typeof module != "undefined" && module.exports)
-  module.exports = MOTO;
+  module.exports = MOTO_FILE ;
