83d9b700bd1f16161aed2d27a5bd4bdbf23b0529
Andrew Ribicki <aribicki@saintjoe.edu>
2015-03-20 08:59:27 -0500

    changed my rebuilt to hours able to ride before rebuild

--- moto.js
+++ moto.js
@@ -4,14 +4,14 @@
 // data for mountains repository for hw to show table
 
 var MOTO = [
-  {name: "HD Heritage Classic", year: 1/1/1992, rebuilt: 1},
-  {name: "Honda CR", year: 1/1/1997, rebuilt: 1},
-  {name: "DR-Z L", year: 1/1/2005, rebuilt: 0},
-  {name: "HD Superglide Custom", year: 1/1/2012 , rebuilt: 0},
-  {name: "KTM XC", year: 1/1/2013, rebuilt: 1},
-  {name: "Honda CRF",  year: 1/1/2014, rebuilt: 0},
-  {name: "Kawasaki KX", year: 1/1/2015, rebuilt: 1},
-  {name: "Ducati", year: 1/1/2015, rebuilt: 0}
+  {name: "HD Heritage Classic", year: 1/1/1992, rebuilt: 150},
+  {name: "Honda CR", year: 1/1/1997, rebuilt: 40},
+  {name: "DR-Z L", year: 1/1/2005, rebuilt: 200},
+  {name: "HD Superglide Custom", year: 1/1/2012 , rebuilt: 150},
+  {name: "KTM XC", year: 1/1/2013, rebuilt: 40},
+  {name: "Honda CRF",  year: 1/1/2014, rebuilt: 60},
+  {name: "Kawasaki KX", year: 1/1/2015, rebuilt: 35},
+  {name: "Ducati", year: 1/1/2015, rebuilt: 100}
 ];
 
 if (typeof module != "undefined" && module.exports)
